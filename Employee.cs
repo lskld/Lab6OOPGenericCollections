140f4dc020004019ff6371440929fc8b793fc9bd
theo.lillieskold@gmail.com <theo.lillieskold@gmail.com>
2025-10-03 13:20:56 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6OOPGenericCollections
+{
+    internal class Employee
+    {
+        public string EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public decimal Salary { get; set; }
+        public Employee(string id, string name, string gender, decimal salary)
+        {
+            EmployeeId = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{EmployeeId} - {Name} ({Gender}) earns {Salary} SEK";
+        }
+          
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Lab6OOPGenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeStack = new Stack<Employee>();
+
+            var employee1 = new Employee("ID4034", "Jonas", "Male", 45000m);
+            var employee2 = new Employee("ID7281", "Anna", "Female", 39500m);
+            var employee3 = new Employee("ID5127", "Erik", "Male", 56200m);
+            var employee4 = new Employee("ID9462", "Sara", "Female", 42100m);
+            var employee5 = new Employee("ID3845", "Lars", "Male", 51000m);
+
+            employeeStack.Push(employee1);
+            employeeStack.Push(employee2);
+            employeeStack.Push(employee3);
+            employeeStack.Push(employee4);
+            employeeStack.Push(employee5);
+
+            Console.WriteLine("Retrieve entire Stack:");
+            foreach (var employee in employeeStack)
+            {
+                Console.WriteLine(employee);
+                Console.WriteLine($"Items left in stack: {employeeStack.Count}");
+            }
+
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine("\nRetrieve using Pop Method:");
+            for (int i = employeeStack.Count; i > 0; i--)
+            {
+                Console.WriteLine(employeeStack.Pop());
+                Console.WriteLine($"Items left in stack: {employeeStack.Count}");
+            }
+
+            employeeStack.Push(employee1);
+            employeeStack.Push(employee2);
+            employeeStack.Push(employee3);
+            employeeStack.Push(employee4);
+            employeeStack.Push(employee5);
+
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine("\nRetrieve using Peek Method:");
+
+            for(int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(employeeStack.Peek());
+                Console.WriteLine($"Items left in stack: {employeeStack.Count}");
+            }
+
+            Console.WriteLine("\n-------------------------------------");
+            var array = employeeStack.ToArray();
+            
+            if(array.Length > 3)
+            {
+                Console.WriteLine("\nObject 3 is in Stack");
+            }
+            else
+            {
+                Console.WriteLine("\nObject number 3 is not in Stack");
+            }
+
+
+        }
+    }
+}
